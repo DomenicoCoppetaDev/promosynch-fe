bfddfc132b5dcddd21cfee8000c5ff47d1d919fd
DomenicoCoppeta <domenico.coppeta.dev@gmail.com>
2024-01-06 00:56:04 +0100

    05/01/24

--- src/components/Happenings/HappeningsList.jsx
+++ src/components/Happenings/HappeningsList.jsx
@@ -1,1 +1,14 @@
+import { ListGroup } from 'react-bootstrap'
+import SingleHappening from './SingleHappening.jsx';
+
+export default function HappeningsList ({ happenings }) {
+  return (
+  <ListGroup className="mt-2">
+    {happenings.map((happening) => (
+      <SingleHappening happening={happening} key={happenig._id}/>
+    ))}
+  </ListGroup>
+  )
+}
+
 
